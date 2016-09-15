e2ddf0c9c3e3165c8d8e3c3d3e881b208c69ac18
Eric Ray <erichamion@gmail.com>
2016-09-15 15:29:33 -0500

    Copied PalindromesByDescendingMultiples into common project

---
+++ PalindromeCommon2/DescendingMultiples/IBestIndexFinder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Palindromes.DescendingMultiples
+{
+    public interface IBestIndexFinder<T>
+    {
+        int FindIndex(IEnumerable<T> list);
+    }
+}
---
+++ PalindromeCommon2/DescendingMultiples/Program.cs
@@ -0,0 +1,24 @@
+﻿using PalindromeCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes.DescendingMultiples
+{
+    public class DescendingMultipleValueFinderFactory : IValueFinderFactory
+    {
+        public ValueFinderCreator CreateValueFinder { get; } =
+            x => new FirstValidValueFinder(new DescendingMultipleSupplier(x), new PalindromeChecker());
+
+        
+
+        public void Run()
+        {
+            new PalindromeUI(this).RunUI();
+        }
+
+        
+    }
+}
