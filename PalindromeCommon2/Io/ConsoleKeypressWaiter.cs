cab3e67c3fb0ece0a569e79da3c9c8d2cbe090e1
Eric Ray <erichamion@gmail.com>
2016-09-16 15:52:39 -0500

    Added ConsoleKeypressWaiter class

---
+++ PalindromeCommon2/Io/ConsoleKeypressWaiter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes.Io
+{
+    class ConsoleKeypressWaiter : IUserInteractionWaiter
+    {
+        public void WaitForUserInteraction()
+        {
+            Console.ReadKey(true);
+        }
+    }
+}
