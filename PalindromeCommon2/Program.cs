8fc2bc8b0addd6a4ffb64b7d77f50fa962016530
Eric Ray <erichamion@gmail.com>
2016-09-17 14:51:30 -0500

    Renamed PalindromeCommon to Palindromes

--- PalindromeCommon2/Program.cs
+++ PalindromeCommon2/Program.cs
@@ -12,12 +12,13 @@ namespace PalindromeCommon
         {
             Tuple<String, IValueFinderFactory>[] strategies =
             {
+                new Tuple<string, IValueFinderFactory>("Generate all multiples of two N-digit numbers in arbitrary order, test each for palindromicity, and keep the highest palindrome found.",
+                                                        new Palindromes.BruteForce.BruteForceValueFinderFactory()),
                 new Tuple<string, IValueFinderFactory>("Generate multiples in descending order, and test for palindromicity until the first palindrome is found.",
                                                         new Palindromes.DescendingMultiples.DescendingMultipleValueFinderFactory()),
                 new Tuple<string, IValueFinderFactory>("Generate palindromes in descending order, and test for factorability until the first multiple of two N-digit numbers is found.",
                                                         new Palindromes.DescendingPalindromes.DescendingPalindromeValueFinderFactory()),
-                new Tuple<string, IValueFinderFactory>("Generate all multiples of two N-digit numbers in arbitrary order, test each for palindromicity, and keep the highest palindrome found.",
-                                                        new Palindromes.BruteForce.BruteForceValueFinderFactory()),
+                
             };
 
             PalindromeUI ui = new PalindromeUI(strategies);
